30bfea1b4132b990dd15d0630a00ae629345cda3
DavidPaniagua15 <davidpaniagua15U@outlook.com>
2022-09-18 13:46:48 -0600

    challenge04

---
+++ Main/assets/js/questions.js
@@ -0,0 +1,28 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'What does HTML stand for?',
+    choices: ['Hyper Trainer Marking Lenguage', 'Hyper Text Marketing Language', 'Hyper Text Markup Language', 'Hyper Text Markup Leveler'],
+    answer: 'Hyper Text Markup Language',
+  },
+  {
+    title: 'Which of the following is not a type of computer code related to Program Execution?',
+    choices: ['Source code', 'Bytecode', 'Machine Code', 'Hex Code'],
+    answer: 'Hex Code',
+  },
+  {
+    title: 'Python is _____ programming language.',
+    choices: ['high-level', 'mid-level', 'low-level', 'none of the above'],
+    answer: 'high-level',
+  },
+  {
+    title: 'Which of the following is not a programming language?',
+    choices: ['TypeScritp', 'Phyton', 'Anaconda', 'Java'],
+    answer: 'Anaconda',
+  },
+  {
+    title: 'Which of the following includes Chromes V8 JavaScript Engine?',
+    choices: ['jQuery', 'Java', 'npm', 'Node.js'],
+    answer: 'Node.js',
+  },
+];
